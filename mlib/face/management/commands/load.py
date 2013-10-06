96accbce07b1c95b84f366699734732f92e51ea8
Mikhail Veltishchev <mvel@yandex-team.ru>
2013-10-06 14:57:26 +0400

    Apply fixups for some broken records

--- mlib/face/management/commands/load.py
+++ mlib/face/management/commands/load.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from django.core.management.base import BaseCommand, CommandError
 from face.models import Book
 
@@ -14,9 +16,12 @@ class Command(BaseCommand):
 
         file_name = args[0]
         f = open(file_name, 'r')
-        fo = open('list-conv.txt', 'w')
-
+        book_count = 0
         for line in f:
+            line = line.strip()
+            if not line:
+                continue
+
             line = line.decode('utf8')
             al = line.split('\t')
             # al contain quoted values, we need to dequote them
@@ -24,32 +29,67 @@ class Command(BaseCommand):
             btok = ''
             for tok in al:
                 tok = tok.strip()
-                print "tok=QQQ" + tok + "ZZZ"
                 if tok[0:1] == '"':
                     tok = tok[1:len(tok) - 1]
                 tok = tok.replace('""', '"')
                 tok = tok.replace('""', '"')
                 bl.append(tok)
 
-            for x in bl:
-                fo.write(x.encode('utf8') + '\t')
-            fo.write('\n')
-            sys.exit(0)
+            book_file_name = bl[7]
+            # skip phantom book
+            if '20070623_D41D8CD9' in book_file_name:
+                continue
+
+            if len(bl) < 9:
+                print bl
+                sys.exit(1)
+
+            # fix swapped year and publication
+            if bl[0] == '43726':
+                tmp = bl[5]
+                bl[5] = bl[6]
+                bl[6] = tmp
+
+            # fix shifted book
+            if bl[0] == '74707':
+                bl[2] = u'Голованов Н.'
+                bl[3] = bl[4]
+                bl[4] = bl[5]
+                bl[5] = bl[6]
+                bl[6] = bl[7]
+                bl[7] = bl[8]
+                bl[8] = bl[9]
+
+            # fix shifted book
+            if bl[0] == '75275':
+                bl[1] = u'Акимушкин И.И.'
+                bl[2] = ''
+                bl[3] = ''
+                bl[4] = bl[5]
+                bl[5] = bl[6]
+                bl[6] = bl[7]
+                bl[7] = bl[8]
+                bl[8] = bl[9]
+
+            try:
+                b = Book(
+                        num=bl[0],
+                        author1 = bl[1],
+                        author2=bl[2],
+                        author3=bl[3],
+                        title=bl[4],
+                        year=bl[5],
+                        publication=bl[6],
+                        file_name=bl[7],
+                        ed2k_hash=bl[8]
+                        )
+                b.save()
+                book_count += 1
+            except BaseException as e:
+                print "There is a problem with book:"
+                print bl
+                print e
+                print '----------------------'
 
         f.close()
-        fo.close()
-
-
-
-        """
-        for  in args:
-            try:
-                poll = Poll.objects.get(pk=int(poll_id))
-            except Poll.DoesNotExist:
-                raise CommandError('Poll "%s" does not exist' % poll_id)
-
-            poll.opened = False
-            poll.save()
-
-            self.stdout.write('Successfully closed poll "%s"' % poll_id)
-        """
+        self.stdout.write('Successfully processed "%d"' % book_count)
--- mlib/face/models.py
+++ mlib/face/models.py
@@ -2,11 +2,12 @@ from django.db import models
 
 class Book(models.Model):
     num = models.IntegerField()
-    author1 = models.CharField(max_length=50)
-    author2 = models.CharField(max_length=50)
-    author3 = models.CharField(max_length=50)
-    title = models.CharField(max_length=200)
-
-    question = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
-
+    author1 = models.CharField(max_length=80)
+    author2 = models.CharField(max_length=80)
+    author3 = models.CharField(max_length=80)
+    title = models.CharField(max_length=300)
+    year =  models.CharField(max_length=4)
+    publication = models.CharField(max_length=100)
+    file_name = models.CharField(max_length=30)
+    ed2k_hash = models.CharField(max_length=32)
+    language =  models.CharField(max_length=2)
