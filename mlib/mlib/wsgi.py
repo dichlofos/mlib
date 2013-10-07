781db1b1a915c88a25089c0a96c29cc716db6c69
Mikhail Veltishchev <mvel@yandex-team.ru>
2013-10-07 11:56:03 +0400

    WSGI imports fixed to make it working from apache

--- mlib/mlib/wsgi.py
+++ mlib/mlib/wsgi.py
@@ -14,6 +14,10 @@ framework.
 
 """
 import os
+import sys
+
+sys.path.append('/var/www/dj/mlib.local')
+sys.path.append('/var/www/dj/mlib.local/mlib')
 
 # We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
 # if running multiple sites in the same mod_wsgi process. To fix this, use
