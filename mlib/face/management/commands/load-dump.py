94049c9d264e5857dfbf4d2e7fe8c9eb4c890d05
Mikhail Veltishchev <mvel@yandex-team.ru>
2013-11-16 11:52:29 +0400

    Refactor load-dump to look like PEP8-code.

--- mlib/face/management/commands/load-dump.py
+++ mlib/face/management/commands/load-dump.py
@@ -1,98 +1,104 @@
 # -*- coding: utf-8 -*-
+""" Load books from TSV dump converted to utf8 encoding """
 
 from django.core.management.base import BaseCommand, CommandError
 from face.models import Book
 
-import os
 import sys
 
+def apply_fixes(elem):
+    """ Fix some invalid lines """
+    # fix swapped year and publication
+    if elem[0] == '43726':
+        tmp = elem[5]
+        elem[5] = elem[6]
+        elem[6] = tmp
+
+    # fix shifted book
+    if elem[0] == '74707':
+        elem[2] = u'Голованов Н.'
+        elem[3] = elem[4]
+        elem[4] = elem[5]
+        elem[5] = elem[6]
+        elem[6] = elem[7]
+        elem[7] = elem[8]
+        elem[8] = elem[9]
+
+    # fix shifted book
+    if elem[0] == '75275':
+        elem[1] = u'Акимушкин И.И.'
+        elem[2] = ''
+        elem[3] = ''
+        elem[4] = elem[5]
+        elem[5] = elem[6]
+        elem[6] = elem[7]
+        elem[7] = elem[8]
+        elem[8] = elem[9]
+
+
 class Command(BaseCommand):
+    """ Django command """
     args = '<file_name>'
     help = 'Loads books into library from dump'
 
     def handle(self, *args, **options):
+        """ Command handler """
         if not args or not args[0]:
             raise CommandError('File name to load does not specified')
 
         file_name = args[0]
-        f = open(file_name, 'r')
+        input_handle = open(file_name, 'r')
         book_count = 0
-        for line in f:
+        for line in input_handle:
             line = line.strip()
             if not line:
                 continue
 
             line = line.decode('utf8')
-            al = line.split('\t')
-            # al contain quoted values, we need to dequote them
-            bl = []
-            btok = ''
-            for tok in al:
+            elements = line.split('\t')
+            # elements contain quoted values, we need to dequote them
+            elem = []
+            for tok in elements:
                 tok = tok.strip()
                 if tok[0:1] == '"':
                     tok = tok[1:len(tok) - 1]
                 tok = tok.replace('""', '"')
                 tok = tok.replace('""', '"')
-                bl.append(tok)
+                elem.append(tok)
 
-            if len(bl) < 8:
-                print bl
+            if len(elem) < 8:
+                print elem
                 sys.exit(1)
 
-            book_file_name = bl[7]
+            book_file_name = elem[7]
             # skip phantom book
             if '20070623_D41D8CD9' in book_file_name:
                 continue
 
-            if len(bl) < 9:
-                print bl
+            if len(elem) < 9:
+                print elem
                 sys.exit(1)
 
-            # fix swapped year and publication
-            if bl[0] == '43726':
-                tmp = bl[5]
-                bl[5] = bl[6]
-                bl[6] = tmp
-
-            # fix shifted book
-            if bl[0] == '74707':
-                bl[2] = u'Голованов Н.'
-                bl[3] = bl[4]
-                bl[4] = bl[5]
-                bl[5] = bl[6]
-                bl[6] = bl[7]
-                bl[7] = bl[8]
-                bl[8] = bl[9]
-
-            # fix shifted book
-            if bl[0] == '75275':
-                bl[1] = u'Акимушкин И.И.'
-                bl[2] = ''
-                bl[3] = ''
-                bl[4] = bl[5]
-                bl[5] = bl[6]
-                bl[6] = bl[7]
-                bl[7] = bl[8]
-                bl[8] = bl[9]
+            apply_fixes(elem)
 
             try:
-                b = Book(
-                        num=bl[0],
-                        author1 = bl[1],
-                        author2=bl[2],
-                        author3=bl[3],
-                        title=bl[4],
-                        year=bl[5],
-                        publication=bl[6],
-                        file_name=bl[7],
-                        ed2k_hash=bl[8])
-                b.save()
+                book = Book(
+                        num=elem[0],
+                        author1 = elem[1],
+                        author2=elem[2],
+                        author3=elem[3],
+                        title=elem[4],
+                        year=elem[5],
+                        publication=elem[6],
+                        file_name=elem[7],
+                        ed2k_hash=elem[8])
+                book.save()
                 book_count += 1
-            except BaseException as e:
+            except BaseException as exc:
                 print "There is a problem with book:"
-                print bl
-                print e
+                print elem
+                print exc
                 print '----------------------'
 
-        f.close()
+        input_handle.close()
         self.stdout.write('Successfully processed "%d"' % book_count)
