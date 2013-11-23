af0a2debd1dc8e90b254c7028395f38c868edb18
Mikhail Veltishchev <mvel@yandex-team.ru>
2013-11-23 20:37:06 +0400

    Improved copier

--- mlib/face/management/commands/export-books.py
+++ mlib/face/management/commands/export-books.py
@@ -7,24 +7,27 @@ from django.core.management.base import BaseCommand, CommandError
 from face.models import Book
 import uuid
 
-BOOKS_DEST = '/var/run/media/disk'
+BOOKS_DEST = '~/disk'
 BOOKS_SOURCE = '/storage/whiterose/libraries/lib.mexmat.ru/Lib'
 
 
 def export_book(book, cat, script):
     """ Export one book """
 
+    dest_root = BOOKS_DEST + '/lmr-books'
+
     dir_name = str(book.id % 512)
-    command = 'mkdir -p ' + BOOKS_DEST + '/' + dir_name
+    command = 'mkdir -p ' + dest_root + '/' + dir_name
     script.write(command + '\n')
     download_name = str(uuid.uuid4()) + '.' + book.ext()
+    html_path = dir_name + '/' +  download_name
 
     command = 'cp ' + BOOKS_SOURCE + '/' + book.path() + \
-        ' ' + BOOKS_DEST + '/' + dir_name + '/' +  download_name
+        ' ' + dest_root + '/' + html_path
 
     script.write(command + '\n')
 
-    link = '<li><a href="%s">%s</a></li>' % (download_name, unicode(book))
+    link = '<li><a href="%s">%s</a></li>' % (html_path, unicode(book))
     cat.write(link.encode('utf-8') + '\n')
 
 
@@ -45,7 +48,10 @@ class Command(BaseCommand):
         cat = open(output_catalog, 'w')
         script = open(copy_script, 'w')
 
-        cat.write('<DOCTYPE html>\n<html>\n' + \
+        script.write('#!/usr/bin/env bash\n')
+        script.write('set -xe\n')
+
+        cat.write('<!DOCTYPE html>\n<html>\n' + \
             '<head>\n<meta charset="utf-8"/>\n' + \
             '<title>Some books</title>\n</head>\n<body>\n')
 
